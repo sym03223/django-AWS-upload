d7384d85274382c4a1d56f4c49b1c2fd58096194
Inhyeok Park <sym03223@naver.com>
2022-02-16 17:04:52 +0900

    update

--- main/templates/main/Winwheel.js
+++ main/templates/main/Winwheel.js
@@ -1,34 +1,4 @@
-/*
-    Winwheel.js, by Douglas McKechie @ www.dougtesting.net
-    See website for tutorials and other documentation.
 
-    The MIT License (MIT)
-
-    Copyright (c) 2012-2019 Douglas McKechie
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
-
-// ====================================================================================================================
-// The constructor for the WinWheel object, a JSON-like array of options can be passed in.
-// By default the wheel is drawn if canvas object exists on the page, but can pass false as second parameter if don't want this to happen.
-// ====================================================================================================================
 function Winwheel(options, drawWheel)
 {
     defaultOptions = {
@@ -2056,7 +2026,9 @@ function Segment(options)
         'textLineWidth'     : null,
         'image'             : null, // Name/path to the image
         'imageDirection'    : null, // Direction of the image, can be set globally for the whole wheel.
-        'imgData'           : null  // Image object created here and loaded with image data.
+        'imgData'           : null,  // Image object created here and loaded with image data.
+        'num'               : null,
+        'count'             : null
     };
 
     // Now loop through the default options and create properties of this class set to the value for
